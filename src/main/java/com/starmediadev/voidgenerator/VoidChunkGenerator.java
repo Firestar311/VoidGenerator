eee3a078dbbef91c823f03a2ed966b933fad058f
Firestar311 <firestar311mc@gmail.com>
2021-06-19 19:47:57 -0500

    Created a void generator plugin

---
+++ src/main/java/com/starmediadev/voidgenerator/VoidChunkGenerator.java
@@ -0,0 +1,18 @@
+package com.starmediadev.voidgenerator;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.generator.ChunkGenerator;
+
+import java.util.Random;
+
+public class VoidChunkGenerator extends ChunkGenerator {
+
+    public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
+        return createChunkData(world);
+    }
+
+    public Location getFixedSpawnLocation(World world, Random random) {
+        return new Location(world, 0, 61, 0);
+    }
+}
---
+++ src/main/java/com/starmediadev/voidgenerator/VoidGeneratorPlugin.java
@@ -0,0 +1,29 @@
+package com.starmediadev.voidgenerator;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VoidGeneratorPlugin extends JavaPlugin implements Listener {
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        var world = Bukkit.getWorld("world");
+        var block = world.getBlockAt(0, 60, 0);
+        block.setType(Material.BEDROCK);
+        e.getPlayer().teleport(new Location(world, 0, 61, 0));
+    }
+
+    public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        return new VoidChunkGenerator();
+    }
+}
