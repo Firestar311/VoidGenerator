54905768997797ecd2d742cce745cfc5698966db
Firestar311 <slaterandrew0398@gmail.com>
2022-02-21 15:03:32 -0600

    Updated to 1.18

--- src/main/java/com/starmediadev/plugins/voidgenerator/VoidChunkGenerator.java
+++ src/main/java/com/starmediadev/plugins/voidgenerator/VoidChunkGenerator.java
@@ -7,11 +7,6 @@ import org.bukkit.generator.ChunkGenerator;
 import java.util.Random;
 
 public class VoidChunkGenerator extends ChunkGenerator {
-
-    public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
-        return createChunkData(world);
-    }
-
     public Location getFixedSpawnLocation(World world, Random random) {
         return new Location(world, 0, 61, 0);
     }
