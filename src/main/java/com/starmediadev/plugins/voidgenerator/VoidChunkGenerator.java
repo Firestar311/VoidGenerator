7a744d0b6bff7e5439b3837c2083322880ed8f93
Firestar311 <slaterandrew0398@gmail.com>
2026-08-02 11:27:15 -0500

    Updated for recent things

--- src/main/java/com/starmediadev/plugins/voidgenerator/VoidChunkGenerator.java
+++ src/main/java/com/starmediadev/plugins/voidgenerator/VoidChunkGenerator.java
@@ -1,12 +1,23 @@
 package com.starmediadev.plugins.voidgenerator;
 
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.generator.ChunkGenerator;
 
 import java.util.Random;
 
 public class VoidChunkGenerator extends ChunkGenerator {
+    
+    @Override
+    public ChunkData generateChunkData(World world, Random random, int x, int z, BiomeGrid biome) {
+        ChunkData chunkData = createChunkData(world);
+        
+        for (int y = 0; y < 256; y++) {
+            chunkData.setBlock(x, y, z, Material.AIR);
+        }
+        
+        return chunkData;
+    }
+    
     public Location getFixedSpawnLocation(World world, Random random) {
         return new Location(world, 0, 61, 0);
     }
--- src/main/java/com/starmediadev/plugins/voidgenerator/VoidGeneratorPlugin.java
+++ src/main/java/com/starmediadev/plugins/voidgenerator/VoidGeneratorPlugin.java
@@ -1,38 +1,12 @@
 package com.starmediadev.plugins.voidgenerator;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntitySpawnEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class VoidGeneratorPlugin extends JavaPlugin implements Listener {
-
-    public void onEnable() {
-        getServer().getPluginManager().registerEvents(this, this);
-    }
-    
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e) {
-        if (Bukkit.getPluginManager().getPlugin("StarEssentials") == null) {
-            var world = Bukkit.getWorld("world");
-            var block = world.getBlockAt(0, 60, 0);
-            block.setType(Material.BEDROCK);
-            e.getPlayer().teleport(new Location(world, 0, 61, 0));
-        }
-    }
-    
-    @EventHandler
-    public void onEntitySpawn(EntitySpawnEvent e) {
-        //e.setCancelled(true);
-    }
+public class VoidGeneratorPlugin extends JavaPlugin {
 
     public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
+        getLogger().info("getDefaultWorldGenerator");
         return new VoidChunkGenerator();
-        //return new WaterChunkGenerator();
     }
 }
